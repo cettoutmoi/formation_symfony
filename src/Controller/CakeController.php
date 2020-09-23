1092c9f457b6fee39b3fefda6290acb4bcb00392
Stéphane DELISLE <SDELISLE@nordpasdecalais.fr>
2020-09-23 17:10:03 +0200

    factory

--- src/Controller/CakeController.php
+++ src/Controller/CakeController.php
@@ -6,16 +6,53 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CakeController extends AbstractController {
 
+    private $pdo;
+
+    private function getPdo()
+{
+    if (null === $this->pdo) {
+        $this->pdo = new \PDO($this->getParameter('pdo_dsn'));
+    }
+
+    return $this->pdo;
+}
+
     public function list()
     {
-        $pdo = new \PDO('sqlite:'. __DIR__ .'/../../var/cakes.sqlite');
 
-        $cakes = $pdo
+        $cakes = $this->getPdo()
             ->query('SELECT * FROM cake ORDER BY created_at DESC')
             ->fetchAll()
         ;
 
         return $this->render('list.html.twig',['cakes'=>$cakes]);
     }
+
+
+    public function show($cakeId)
+{
+    $cake = $this->getPdo()
+        ->query(sprintf('SELECT * FROM cake WHERE id = %d', $cakeId))
+        ->fetch()
+    ;
+    if (!$cake) {
+        throw $this->createNotFoundException(sprintf('The cake with id "%s" was not found.', $cakeId));
+    }
+
+    $categories = $this->getPdo()
+        ->query(sprintf('
+            SELECT * FROM category
+            INNER JOIN cake_category ON category.id = cake_category.category_id
+            WHERE cake_category.cake_id = %d
+        ', $cakeId))
+        ->fetchAll()
+    ;
+    
+    return $this->render('show.html.twig', [
+        'cake' => $cake,
+        'categories' => $categories,
+    ]);
+}
+
 
 }
